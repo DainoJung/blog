e469c0115929ef399e7ec7ff85a9f1df0ac30157
DainJung <wjdekdls3693@gmail.com>
2023-10-15 12:11:58 +0900

    post BottomNavigationBar

---
+++ blog/lib/s_dashboard.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  late int index;
+  bool isPushOn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: homebody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'person'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() {
+          index = newIndex;
+        }),
+      ),
+    );
+  }
+
+  Widget homebody() {
+    switch (index) {
+      case 1:
+        return const Center(child: Icon(Icons.search, size: 100));
+      case 2:
+        return const Center(child: Icon(Icons.person, size: 100));
+      case 0:
+      default:
+        return const Center(child: Icon(Icons.home, size: 100));
+    }
+  }
+}
